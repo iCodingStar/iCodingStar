da6679fa654e6803947fc5dcfc369215e9a7eb42
iCodingStar <shixing@codingstar.cn>
2016-12-30 10:46:47 +0800

    log4j config

---
+++ ics-blog/src/main/java/cn/codingstar/blog/web/controller/HomePageController.java
@@ -0,0 +1,22 @@
+package cn.codingstar.blog.web.controller;
+
+/**
+ * Description: HomePageController
+ *
+ * @author iCodingStar
+ * @version 1.0
+ *          Filename:    HomePageController.java
+ *          Create at:   2016/12/29
+ *          <p>
+ *          Copyright:   Copyright (c)2016
+ *          Company:     songxiaocai
+ *          <p>
+ *          Modification History:
+ *          Date                  Author         Version    Description
+ *          ------------------------------------------------------------------
+ *          2016/12/29           iCodingStar       1.0       1.0 Version
+ */
+public class HomePageController {
+
+
+}
