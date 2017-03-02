59d2c62f7145b37e32bf391045c6b7a986732366
iCodingStar <shixing@codingstar.cn>
2017-03-02 17:29:10 +0800

    update webUser

--- ics-api/src/main/java/cn/codingstar/api/controller/user/WebUserController.java
+++ ics-api/src/main/java/cn/codingstar/api/controller/user/WebUserController.java
@@ -1,5 +1,13 @@
 package cn.codingstar.api.controller.user;
 
+import cn.codingstar.common.result.ApiResult;
+import cn.codingstar.model.persistent.WebUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.xml.ws.RequestWrapper;
+
 /**
  * Description: WebUserController
  *
@@ -15,5 +23,31 @@ package cn.codingstar.api.controller.user;
  *          ------------------------------------------------------------------
  *          2017/3/2           iCodingStar       1.0       1.0 Version
  */
+@Controller
+@RequestMapping("/webUser")
 public class WebUserController {
+
+    @RequestMapping(value = {"/register"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public ApiResult<WebUser> register() {
+        ApiResult<WebUser> apiResult = new ApiResult<>();
+        WebUser webUser = new WebUser();
+        webUser.setId(1000);
+        webUser.setNickname("iCodingStar");
+        apiResult.setCode(200);
+        apiResult.setMessage("注册成功！");
+        apiResult.setData(webUser);
+        return apiResult;
+    }
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+    public ApiResult<WebUser> login() {
+        ApiResult<WebUser> apiResult = new ApiResult<>();
+        WebUser webUser = new WebUser();
+        webUser.setId(1000);
+        webUser.setNickname("iCodingStar");
+        apiResult.setCode(200);
+        apiResult.setMessage("登陆成功！");
+        apiResult.setData(webUser);
+        return apiResult;
+    }
 }
